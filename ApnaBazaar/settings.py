665141776d6e83f989ced7c1384cb21f83e2e47e
Paras Yadav <99320150+ParasTechno@users.noreply.github.com>
2022-03-13 22:08:45 +0530

    Update settings.py

--- ApnaBazaar/settings.py
+++ ApnaBazaar/settings.py
@@ -135,5 +135,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "kartik892961@gmail.com"
-EMAIL_HOST_PASSWORD = "892961kartik"
+EMAIL_HOST_PASSWORD = "password"
 
