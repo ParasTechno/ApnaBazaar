09545bc73ed8f99e0d0d1cdb276ed05791f43363
Paras Yadav <99320150+ParasTechno@users.noreply.github.com>
2022-03-15 14:53:40 +0530

    Update settings.py

--- ApnaBazaar/settings.py
+++ ApnaBazaar/settings.py
@@ -134,6 +134,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "kartik892961@gmail.com"
-EMAIL_HOST_PASSWORD = "892961kartik"
+EMAIL_HOST_USER = "yourmail@gmail.com"
+EMAIL_HOST_PASSWORD = "your password"
 
